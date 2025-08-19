e3a4a31f516bbf99c5d8d9eb0e5313851be79963
upx-dev-android-02 <otaviodev@upxtec.com.br>
2025-08-19 16:24:07 -0300

    first commit

---
+++ n.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3050;
+
+app.get('/', (req, res) => {
+  res.send('Bom dia');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
