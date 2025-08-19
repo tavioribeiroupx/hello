c35a5a149e988c0161e09217e70b98dd33786915
upx-dev-android-02 <otaviodev@upxtec.com.br>
2025-08-19 17:21:08 -0300

    Adiciona workflow para build local

--- n.js
+++ n.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3050;
 
 app.get('/', (req, res) => {
-  res.send('Bom dia tudo bem, Samuel?');
+  res.send('Bom dia tudo bem, Usuário?');
 });
 
 app.listen(port, () => {
