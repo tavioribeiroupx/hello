355ea2a2ddb843c10a4d226e8e1ba714aa335b60
upx-dev-android-02 <otaviodev@upxtec.com.br>
2025-08-20 10:06:25 -0300

    jwj

--- n.js
+++ n.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3050;
 
 app.get('/', (req, res) => {
-  res.send('Bom dia mundo!wdwdwddw');
+  res.send('Bom dia mundo!tfhtfhfthft');
 });
 
 app.listen(port, () => {
